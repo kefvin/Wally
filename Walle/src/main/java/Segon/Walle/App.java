d6ff105530e942bfeff2a998cf74ba5637ac2aa5
Kevin <kevinappenrodt14@gmail.com>
2016-11-28 19:55:06 +0100

    A media busqueda

--- Walle/src/main/java/Segon/Walle/App.java
+++ Walle/src/main/java/Segon/Walle/App.java
@@ -19,5 +19,35 @@ public class App
     public static void main( String[] args )throws IOException
     {
     	// expresión regular a usar = \WWally\W
+    	
+    	BufferedReader br = new BufferedReader( new InputStreamReader(
+    			App.class.getResource("text.txt").openStream()));
+    	
+    	String linia;
+    	int total = 0;
+    	
+    	while((linia = br.readLine()) != null){
+    		
+			buscar(linia);
+			// if compta > 1 || total > 1
+			
+		}
     }
+    
+	private static void buscar(String linia) {
+		String find = "\\W"+CADENAATROBAR+"\\W";
+		
+		//Matcher es donde buscar y pattern es la expresión regular que busco
+		Pattern p = Pattern.compile(find);
+		Matcher m = p.matcher(linia);
+		
+		int compta = 0; 
+		
+		while(m.find()){
+			compta++;
+		}
+		// return compta
+		
+		// Si hay más de 1 match FITXER INCORRECTE
+	}
 }
