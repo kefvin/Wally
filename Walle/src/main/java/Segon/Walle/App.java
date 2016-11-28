2cb75a4e1aada066727a6a91242a36ab5643bcf5
Kevin <kevinappenrodt14@gmail.com>
2016-11-28 19:33:23 +0100

    Push!

---
+++ Walle/src/main/java/Segon/Walle/App.java
@@ -0,0 +1,23 @@
+package Segon.Walle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private final static String CADENAATROBAR = "Wally";
+    public static void main( String[] args )throws IOException
+    {
+    	// expresión regular a usar = \WWally\W
+    }
+}
